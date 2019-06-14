8ac945488859ca3f17a3c622dc6318de1f425fc4
xiaowei <1412157658@qq.com>
2019-06-14 11:29:52 +0800

    项目优化

--- src/store/cinema/index.js
+++ src/store/cinema/index.js
@@ -15,7 +15,7 @@ export default {
   },
   actions: {
     getCinemaList ({ commit }) {
-      axios.get('http://localhost:8080/ajax/cinemaList?day=2019-06-12&offset=0&limit=20&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=&updateShowDay=true&reqId=1560320616582&cityId=30').then(response => {
+      axios.get('/maoyan/ajax/cinemaList?day=2019-06-12&offset=0&limit=20&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=&updateShowDay=true&reqId=1560320616582&cityId=30').then(response => {
         let res = response.data
         // console.log(res.cinemas)
         commit('SETCINEMALIST', res.cinemas)
